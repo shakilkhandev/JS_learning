91a98525462e139cc276c6cbe2ac17fc71ed2260
Shakil Khan <60468711+shakilkhan2045@users.noreply.github.com>
2024-02-27 22:23:30 +0600

    Add files via upload
    here the pattern are displayed as number started from 0 to n'th as number of row

---
+++ pyramid_pattern_2.js
@@ -0,0 +1,13 @@
+
+rowNumber = 5;
+
+for (let a = 0; a <=rowNumber; a++) {
+    let star="";// variable name randomly picked (ignore it)
+     for (let b = 0; b <a; b++) {
+          star +=b ;
+     }
+     
+     console.log(star); 
+}
+//shakil_khan
+ 
