50d79b254d690af19912673878e87b4385e464d6
Shakil Khan <60468711+shakilkhan2045@users.noreply.github.com>
2024-02-29 01:57:55 +0600

    Add files via upload
    printing  variable and string via ES6 version and older version method

---
+++ string_printing_methods.js
@@ -0,0 +1,11 @@
+const firstname = "shakil khan";
+const  job = "CSE student";
+const  birthyear =1998;
+const  year = 2024;
+const  currently="currently learning javascript";
+
+const info = "I am "+firstname +" a " + job + " and I am "+(year-birthyear)+" old";
+
+const newinfo = `I'm ${firstname} a  ${job} and I am ${year-birthyear} old ${currently} `;
+
+console.log(newinfo);
