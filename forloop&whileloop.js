cb75f8607fa3587853dab80ec8b1f6220450cb8c
Shakil Khan <60468711+shakilkhan2045@users.noreply.github.com>
2024-02-29 02:18:12 +0600

    Add files via upload

---
+++ forloop&whileloop.js
@@ -0,0 +1,12 @@
+const message="For loop is running";
+
+for (let a=0;a<2; a++){
+    console.log(message);
+}
+ 
+const message2="while loop is running";
+let a = 0 ;
+  while(a<2){
+    console.log(message2);
+    a++;
+  }
