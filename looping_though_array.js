ae5d29ef96cfb079b700c7f86f017ada9c4d2422
Shakil Khan <60468711+shakilkhan2045@users.noreply.github.com>
2024-03-06 22:58:19 +0600

    Add files via upload

---
+++ looping_though_array.js
@@ -0,0 +1,7 @@
+const language = ["java","python","C++","Javascript","C"];
+let fullarray=' ';
+for(let i=0;i<language.length;i++){
+    fullarray += language[i]+"  " ;
+    
+}
+console.log(fullarray);
