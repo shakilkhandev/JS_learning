1f2be944f69f5d98c12f3360f32e415ddf81d1b2
Shakil Khan <60468711+shakilkhan2045@users.noreply.github.com>
2024-03-16 22:33:28 +0600

    Create String_method.js
    method example to slice a string from a character position , transform into UpperCase or LowerCase.

---
+++ String_method.js
@@ -0,0 +1,8 @@
+console.log("--------String Methods-------")
+
+let text=`This is a string`;
+x = text.toUpperCase();
+y = text.toLowerCase();
+z = text.slice(8);
+
+console.log("UpperCase()  :    " + x +"\nLowercase()  :    " +y+"\nSliced()at 8th Character :"+ z );
