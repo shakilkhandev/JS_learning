23953c4d5d977cc2c30d8aea0c180ed5070eba06
Shakil Khan <60468711+shakilkhan2045@users.noreply.github.com>
2024-03-20 23:40:30 +0600

    Create sorting_the_array.js
    sorting an array alphabetically and reversely

---
+++ sorting_the_array.js
@@ -0,0 +1,19 @@
+const fruits = ["Apple", "Orange", "Apple", "Mango"];
+console.log("Array of fruits = : " +fruits);
+
+console.log("\n--Inserting another fruits'Banana'as 5th item--");
+fruits [4]="Banana";
+
+console.log(fruits);
+
+fruits.sort();
+
+console.log("--Sorting the array Alphabetically");
+console.log(fruits);
+
+console.log("--Sorting the array (reverse) Alphabetically");
+fruits.reverse();
+console.log(fruits);
+
+
+
