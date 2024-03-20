ea0994ee482f73ef3e745fba33c489d194a021da
Shakil Khan <60468711+shakilkhan2045@users.noreply.github.com>
2024-03-20 23:18:29 +0600

    Update array_search.js

--- array_search.js
+++ array_search.js
@@ -1,9 +1,9 @@
 const fruits = ["Apple", "Orange", "Apple", "Mango"];
 console.log("Array of fruits = :" +fruits);
 
-console.log("Searching for " +fruits[2]);
+console.log("Searching for " +fruits[2]);//fruits[2] is "Apple";
 
-let position = fruits.indexOf(fruits[2]) + 1;
+let position = fruits.indexOf("Apple") + 1;
 console.log("From Start Apple found on : " + position);
 
 position = fruits.lastIndexOf("Apple") + 1;
