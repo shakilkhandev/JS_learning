ea0b2dea0ce7a3c46f7e08f0f7c871b2d6497aa9
Shakil Khan <60468711+shakilkhan2045@users.noreply.github.com>
2024-04-05 18:18:31 +0600

    Create maxNumberfromarray.js
    this block of code perform the task that find the max number from array named x and y  and print them to the console.

---
+++ maxNumberfromarray.js
@@ -0,0 +1,11 @@
+
+console.log("Finding max number from the array  ");
+let y = [10,11,12];
+let x = [50,80,70];
+let max = ("max of 'y' : "+ Math.max(...y ) +"\nMax of 'x' : "+ Math.max(...x));
+console.log("y : " + y);
+console.log("x : " + x);
+console.log(max);
+
+
+
