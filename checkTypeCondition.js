37f3398509fc7499d17e7c57e156b01996dfee0b
Shakil Khan <60468711+shakilkhan2045@users.noreply.github.com>
2024-04-08 17:59:13 +0600

    Create checkTypeCondition.js
     //check if z equal is any string and x equal any number

---
+++ checkTypeCondition.js
@@ -0,0 +1,21 @@
+let x =10;
+let z ='hello_Shakil';
+   
+   
+   //check if z equal is any string and x equal any number 
+if (typeof x=='number' && typeof  z == 'string' ){
+    console.log('condition meets')
+}
+else {
+    console.log('condition not meets')
+};let x =10;
+let z ='hi';
+   
+   
+   //check if z equal is any string and x equal any number 
+if (typeof x=='number' && typeof  z == 'string' ){
+    console.log('condition meets')
+}
+else {
+    console.log('condition not meets')
+};
