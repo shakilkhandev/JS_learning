94c8feb6614dea066378df103483b560e1f89d9b
Shakil Khan <60468711+shakilkhan2045@users.noreply.github.com>
2024-04-12 23:10:51 +0600

    Create randomNumber.js

---
+++ randomNumber.js
@@ -0,0 +1,3 @@
+let randomNum = Math.floor(Math.random()*5); // generating the random number by using the Math.random that used with Math.floor.
+
+console.log("The random number is : " + randomNum);//printing the result 
