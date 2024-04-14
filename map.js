5963e64f6b45616711b141f250f690c599ea0f60
Shakil Khan <60468711+shakilkhan2045@users.noreply.github.com>
2024-04-14 21:43:42 +0600

    Create map.js

---
+++ map.js
@@ -0,0 +1,8 @@
+// Create a Map
+const fruits = new Map([
+  ["dog", 500],
+  ["cat", 300],
+  ["mouse", 200]
+]);
+
+console.log( fruits.get("cat") ) //output in console by calling get method of map.
