01f3e8ea6aa5e93f77d4dcf23f737c07dc363d1a
Shakil Khan <60468711+shakilkhan2045@users.noreply.github.com>
2024-05-03 03:55:47 +0600

    Create personal_info_management(constructor&find).js
    # Personal Information Management( just JS)
    
    This is a simple JavaScript file for managing personal information. It allows users to create, view all, and search for information about individuals, such as their name and age.
    
    
    
    ## Features
    
    - **Create**: Add new individuals with their name and age.
    - **View All**: Display information about all individuals stored in the system.
    - **Find by Name**: Search for individuals by their name and view their details.
    
    ## Usage
    
    1. Open the `index.html` file in a web browser.
    2. Choose one of the following options:
        - **Create**: Enter the name and age of the individual to add.
        - **View All**: See information about all individuals stored in the system.
        - **Find by Name**: Enter a name to search for and view details about the individual.
    3. Follow the on-screen instructions to navigate through the options.

---
+++ personal_info_management(constructor&find).js
@@ -0,0 +1,98 @@
+let count =1;
+
+function person (name,age){
+  
+  this.fullname = name ;
+  this.age   = age ;
+    
+}
+
+const info=[];
+
+function create (){
+        let inputname = prompt('Enter a name :');
+        let inputage = prompt('Enter an age :');
+        const person1 = new person(inputname,inputage);
+        
+        info.push(person1);
+        
+        console.log('Succesfully Created')
+        
+        start();
+        
+    
+}
+
+function showall(){
+     
+    info.forEach(function(stats){
+         
+        console.log(count+".Name :", stats.fullname, 'Age :',stats.age);
+        count+=1;
+    });
+    
+    
+    
+    menu();
+};
+
+function showbyname(name){
+    
+    name = prompt('Input Name :');
+    
+    let check = info.find(function(personfind){
+        return personfind.fullname === name ;
+    });
+    if(check){
+        console.log('Founded...');
+        console.log('Name : ',check.fullname, 'Age : ',check.age);
+        menu();
+    }
+    
+    else {
+        console.log('Not found.');
+       menu();
+    }
+    
+    
+};
+
+function start(){
+    console.log('Enter Choice');
+    console.log('1.Create.\n2.Show-All.\n3.Find by Name.');
+    let choose = prompt() ;
+    if(choose == 1 ){
+        create();
+    }
+     else if(choose == 2 ){
+        showall();
+    }
+     else if(choose == 3 ){
+        showbyname();
+    }
+    
+    else{
+      console.log('Not a valid Choice')
+    }
+    
+    
+    
+}
+
+function menu(){
+     console.log("Enter 1 to go menu.")
+        let menu = prompt();
+        if(menu == 1 ){
+            start();
+        }
+        else{
+           console.log ("exiting...")
+        return 0
+        }
+    
+}
+
+
+start();
+
+
