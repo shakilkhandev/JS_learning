e586d2f9279311575b73fd172ff23bba467ec6aa
Shakil Khan <60468711+shakilkhandev@users.noreply.github.com>
2024-12-20 04:11:59 +0600

    Create rest_parameter.js

---
+++ rest_parameter.js
@@ -0,0 +1,13 @@
+function collectThis ( x,y,...z) {
+
+  
+ console.log(x); //hold 1st aurgument 
+ console.log(y);  // hold 2nd aurgument 
+ console.log(z); //this will hold the value as array if there is more than 2 aurgument .
+
+
+}
+
+
+
+console.log(collectThis(1,2,3,4,5));
