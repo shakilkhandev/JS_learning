5ff0bf4cb997fe4f15834c6fed5af2b6c8d91fe3
shakilkhadev <shakilkhanfv@gmail.com>
2025-07-06 23:36:27 +0600

    practiced again

--- personal_info_management(constructor&find&filter)method.js
+++ personal_info_management(constructor&find&filter)method.js
@@ -1,110 +1,80 @@
-let count =1;
+const readline = require('readline');
 
-function person (name,age){
-  
-  this.fullname = name ;
-  this.age   = age ;
-    
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let count = 1;
+const info = [];
+
+function person(name, age) {
+  this.fullname = name;
+  this.age = age;
 }
 
-const info=[];
-
-function create (){
-        let inputname = prompt('Enter a name :');
-        let inputage = prompt('Enter an age :');
-        const person1 = new person(inputname,inputage);
-        
-        info.push(person1);
-        
-        console.log('Succesfully Created')
-        
-        start();
-        
-    
+function ask(question) {
+  return new Promise(resolve => rl.question(question, resolve));
 }
 
-function showall(){
-     
-    info.forEach(function(stats){
-         
-        console.log(count+".Name :", stats.fullname, 'Age :',stats.age);
-        count+=1;
+async function create() {
+  const inputname = await ask('Enter a name: ');
+  const inputage = await ask('Enter an age: ');
+  const person1 = new person(inputname, inputage);
+  info.push(person1);
+  console.log('Successfully Created\n');
+  await start();
+}
+
+function showall() {
+  count = 1; // reset count each time
+  info.forEach(stats => {
+    console.log(`${count}. Name: ${stats.fullname}, Age: ${stats.age}`);
+    count += 1;
+  });
+  menu();
+}
+
+async function showbyname() {
+  const name = await ask('Input Name: ');
+  const check = info.filter(p => p.fullname === name);
+
+  if (check.length > 0) {
+    check.forEach(p => {
+      console.log(`Name: ${p.fullname}, Age: ${p.age}`);
     });
-    
-    
-    
+  } else {
+    console.log('Not found.');
+  }
+
+  menu();
+}
+
+async function start() {
+  console.log('\nEnter Choice');
+  console.log('1. Create\n2. Show All\n3. Find by Name');
+  const choose = await ask('Choice: ');
+
+  if (choose == '1') {
+    await create();
+  } else if (choose == '2') {
+    showall();
+  } else if (choose == '3') {
+    await showbyname();
+  } else {
+    console.log('Not a valid choice');
     menu();
-};
-
-function showbyname(name){
-    
-    name = prompt('Input Name :');
-    
-    let check = info.filter(function(personfilter){
-        return personfilter.fullname === name ;
-    });
-    
-    if(check.length>0){
-        
-        check.forEach(function(all){
-            console.log("Name :"+all.fullname,"Age :"+all.age);
-        });
-        
-        menu();
-        
-    }
-    
-    // This was used with find();
-    //  if(check.length==1){
-    //     console.log('Founded...');
-    //     console.log('Name : ',check.fullname, 'Age : ',check.age);
-    //     menu();
-    // }
-    
-    else {
-        console.log('Not found.');
-       menu();
-    }
-    
-    
-};
-
-function start(){
-    console.log('Enter Choice');
-    console.log('1.Create.\n2.Show-All.\n3.Find by Name.');
-    let choose = prompt() ;
-    if(choose == 1 ){
-        create();
-    }
-     else if(choose == 2 ){
-        showall();
-    }
-     else if(choose == 3 ){
-        showbyname();
-    }
-    
-    else{
-      console.log('Not a valid Choice')
-    }
-    
-    
-    
+  }
 }
 
-function menu(){
-     console.log("Enter 1 to go menu.")
-        let menu = prompt();
-        if(menu == 1 ){
-            start();
-        }
-        else{
-           console.log ("exiting...")
-        return 0
-        }
-    
+async function menu() {
+  const goBack = await ask('\nEnter 1 to go to menu or any other key to exit: ');
+  if (goBack == '1') {
+    await start();
+  } else {
+    console.log('Exiting...');
+    rl.close();
+  }
 }
-
 
 start();
-
-
