2708c7228819469c4bec9096ce196b617e7c7d82
shakilkhadev <shakilkhanfv@gmail.com>
2025-07-20 14:44:38 +0600

    callback hell example simalated like real app

---
+++ callbackHellRealWorldLikeExample.js
@@ -0,0 +1,177 @@
+let users = [
+    {
+        userID: 1,
+        userName: "shakil khan",
+        posts: [
+            {
+                caption: " I am learing JavaScirpt",
+                comments: [
+                    {
+                        userID: 2,
+                        comment: "Thats great , wish you success",
+                    }
+                ]
+
+            },
+
+            {
+                caption: "This is high time to concern about plastic waste",
+                comments: [
+                    {
+                        userID: 34,
+                        comment: "absolutely , goverment should take proper action ",
+                    }
+                ]
+
+            },
+
+
+
+
+        ]
+    },
+    {
+        userID: 2,
+        userName: "King khan",
+        posts: [
+            {
+                caption: " I am watching a movie",
+                comments: [
+                    {
+                        userID: 3,
+                        comment: "what is the name of the movie?",
+                    }
+                ]
+
+            },
+
+
+
+
+
+
+        ]
+    },
+]
+
+
+//callbackHell example //
+
+/*     
+
+const getUser = (userID, callback) => {
+    setTimeout(() => {
+        let user = users.find((user) => user.userID === userID);
+        console.log("User found :", user.userName);
+
+        callback(user);
+    }, 1000)
+};
+
+const getPost = (user, callback) => {
+
+    let posts = user.posts;
+    setTimeout(() => {
+
+        console.log("User post fetched ....  ");
+        posts.map((post, index) => {
+            console.log("Post :", index, " \n ");
+            console.log(post.caption);
+            callback(post);
+
+            console.log(` \n  \n `)
+        }
+        )
+
+    }, 1500);
+
+
+};
+
+const getComments = (post, callback) => {
+
+    let comments = post.comments;
+    comments.map((commnent) => console.log("User.ID : ", commnent.userID, "\n comment : ", commnent.comment));
+
+    callback("   \n   \n   ")
+
+
+
+}
+
+
+getUser(1, (user) => {
+    getPost(user, (post) => {
+        getComments(post, (endConfirmation) => {
+            console.log(endConfirmation);
+        })
+
+    })
+}
+
+) 
+
+
+ */
+
+
+
+// by using     Promises
+
+
+const getUser = (userID) => {
+    return new Promise((res, rej) => setTimeout(() => {
+
+
+        let user = users.find((user) => user.userID === userID);
+        if (user) {
+
+            console.log("User found :", user.userName);
+
+            res(user);
+        }
+        rej("User not Found");
+
+    }, 1000)
+    )
+};
+
+const getPost = (user) => {
+
+
+
+    let posts = user.posts;
+    if (posts) {
+        setTimeout(() => {
+
+            console.log("User posts fetching ....  ");
+            posts.map((post, index) => {
+                console.log("Post :", index, " \n ");
+                console.log(post.caption);
+                let comments = post.comments;
+
+                if (comments.length > 0) {
+                    comments.map((commnent) => console.log("User.ID : ", commnent.userID, "\n comment : ", commnent.comment));
+
+                    console.log(` \n  \n `)
+                }
+            }
+            )
+
+        }, 1500);
+
+    }
+
+
+};
+
+
+
+
+
+getUser(1)
+    .then((user) => getPost(user))
+    .catch(err => console.log("error:", err))
+
+
+
