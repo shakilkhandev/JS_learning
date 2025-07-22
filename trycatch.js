cdda95e10aaa2a5c04d0072e990cfffe3c4a4e28
shakilkhadev <shakilkhanfv@gmail.com>
2025-07-23 04:32:37 +0600

    exploring some changes

--- trycatch.js
+++ trycatch.js
@@ -1,10 +1,16 @@
 try {
+
+
+     let a = 0 ;
+      a  === 0 && ( ()=>{  throw new Error("can't divide by zero");  })();
     // Code that may throw an error
     let result = 10 / a;
 // This will throw an error because you can't divide by a without declare it 
 
     console.log("Result:" + result ); // This line will not execute because the error occurs before it
-} catch (error) {
+} 
+
+catch (error) {
     // Code to handle the error
     console.log("An error occurred:", error.message); // Print the error message
 }
