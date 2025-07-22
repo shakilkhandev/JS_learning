c2cf4690e1e2d7c3c842e6e31fef693c7650367d
shakilkhadev <shakilkhanfv@gmail.com>
2025-07-23 00:16:39 +0600

    date fn  recap

--- date.js
+++ date.js
@@ -1,4 +1,4 @@
-let time ;
-
-console.log(Date());
+let time = Date.now() ;   //will print the current time in milisecond from January 1st, 1970 at 00:00:00 UTC,
+                             //and  1 second = 1000 ms ;
+console.log(time);
 
